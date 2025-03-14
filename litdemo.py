1f7167108f16b9dc2421a97de4efa344df744c65
buvir <buvaneshr2018@gmail.com>
2025-03-14 19:02:51 +0530

    Changed

--- litdemo.py
+++ litdemo.py
@@ -7,3 +7,27 @@ st.header('Bus Routes')
 
 
 st.subheader('Bus Routes')
+
+st.write ("This is the first page of streamlit")
+
+st.markdown ("""
+# h1
+## h2
+### h3
+
+
+**Hello**
+***Hello***
+             
+:moon:<br>
+             
+:sunglasses:
+             
+             :sunglasses:
+""",True)
+
+st.write("Data","Science")
+
+st.write("# Data")
+
+st.write(st)
