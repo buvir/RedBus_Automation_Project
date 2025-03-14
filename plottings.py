3b1635de2aa5e8b7be7f5e454560167bb98ab09e
buvir <buvaneshr2018@gmail.com>
2025-03-14 20:17:21 +0530

    changed

--- plottings.py
+++ plottings.py
@@ -2,6 +2,7 @@ import streamlit as st
 import  pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import altair as alt
 
 
 data =pd.DataFrame(np.random.randn(100,3),columns=['a','b','c'])
@@ -10,3 +11,20 @@ st.line_chart(data)
 
 
 st.area_chart(data)
+
+st.bar_chart(data)
+
+fig,ax=plt.subplots()
+ax.scatter(data['a'],data['b'])
+ax.set_title('Scatter')
+st.pyplot(fig)
+
+chart=alt.Chart(data).mark_circle().encode(x='a',y='b')
+st.altair_chart(chart)
+
+chart=alt.Chart(data).mark_circle().encode(x='a',y='b')
+st.altair_chart(chart, use_container_width=True)
+
+
+
+
