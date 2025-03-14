8bd58eb68c3d5072d6348d19275c993e6600f569
buvir <buvaneshr2018@gmail.com>
2025-03-14 22:24:44 +0530

    changed

---
+++ media.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('MEDIA')
+
+st.image("C:/Users/USER/Desktop/RedBus_Automation_Project/download (6).jpeg")
+
+st.image("C:/Users/USER/Desktop/RedBus_Automation_Project/download (6).jpeg",width=200)
+
+st.audio("C:/Users/USER/Desktop/RedBus_Automation_Project/11L-wind_ambience-68680896.mp3")
+
+st.video("C:/Users/USER/Desktop/RedBus_Automation_Project/3144446-hd_1920_1080_25fps.mp4")
+
+st.video("https://youtu.be/eyAAUGhvZu8?si=75DUwt4sv3HKygyk4")
+
+st.markdown("[REDBUS](https://www.redbus.in/)")
