747b429cb4ad33c4bc4539380295d566f53660e4
buvir <buvaneshr2018@gmail.com>
2025-03-15 14:30:46 +0530

    changes

--- pagestrealit.py
+++ pagestrealit.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import time 
 
 plt.style.use("ggplot")
 
@@ -26,14 +27,43 @@ df=pd.DataFrame(data=data)
 
 #st.sidebar.selectbox (" select a column ",[1,2,3,4])
 
-col=st.sidebar.selectbox (" select a column ",df.columns)
-st.write(col)
+# col=st.sidebar.selectbox (" select a column ",df.columns)
+# st.write(col)
 
-fig,ax=plt.subplots()
-ax.plot(df['num'],df[col])
+# fig,ax=plt.subplots()
+# ax.plot(df['num'],df[col])
 
-ax.set_title(f'plot of {col} vs Num')
-ax.set_xlabel('num')
-ax.set_ylabel('col')
+# ax.set_title(f'plot of {col} vs Num')
+# ax.set_xlabel('num')
+# ax.set_ylabel('col')
 
-st.pyplot(fig)
+# st.pyplot(fig)
+
+# radio selection
+r=st.sidebar.radio('Navigation',["Home","About_US"])
+if r=='Home':
+    #col=st.sidebar.selectbox (" select a column ",df.columns)
+    col=st.sidebar.multiselect (" select a column ",df.columns)
+    st.write(col)
+
+    fig,ax=plt.subplots()
+    ax.plot(df['num'],df[col])
+
+    ax.set_title(f'plot of {col} vs Num')
+    ax.set_xlabel('num')
+    ax.set_ylabel('col')
+
+    st.pyplot(fig)
+
+if r=="About_US":
+    progress=st.progress(0)
+    for i in range(100):
+        time.sleep(0.1)
+        progress.progress(i+1)
+    st.balloons()
+    st.write('about us page')
+    st.success("super!")
+    st.error("error!")
+    st.exception("exception?")
+    st.warning("warning")
+    st.info("info")
